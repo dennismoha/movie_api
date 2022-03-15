af37e4d369764a013974a1e37d79fa5dc00b799b
Ej <Ejgithinji@gmail.com>
2022-03-15 15:51:50 +0300

    modified moviesReducer and moviestate

---
+++ entertainment/context/movieContext/MoviesReducer.js
@@ -0,0 +1,12 @@
+import { FETCH_MOVIES } from '../ActionTypes';
+
+export default (state, action) => {
+    switch (action.type) {
+        case FETCH_MOVIES:
+            return {
+                ...state,
+
+            }
+    }
+
+}
